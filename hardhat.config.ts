14edd5c38a1acd98cfc9c09dcb3ff9e16988ba50
Peter Graham <peterghm@gmail.com>
2021-08-27 15:04:03 -1000

    try to implement coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-vyper';
+import 'solidity-coverage';
 
 module.exports = {
   solidity: '0.8.4',
--- test/setupJest.ts
+++ test/setupJest.ts
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals';
 import { waffleJest } from '@ethereum-waffle/jest';
 
 jest.setTimeout(10000);
