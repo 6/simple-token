f0cd1deae57376a6d933bfc9d46338557e3faa42
Peter Graham <peterghm@gmail.com>
2021-08-27 17:07:11 -1000

    specify version consitently

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,9 @@ import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-vyper';
 
 module.exports = {
-  solidity: '0.8.4',
+  solidity: {
+    version: '0.8.4',
+  },
   vyper: {
     version: '0.2.15',
   },
