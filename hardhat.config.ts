12ac14245f9e4815f123cf77b9c80767bbfefe8f
Peter Graham <peterghm@gmail.com>
2021-08-28 21:16:37 -1000

    deploy to rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,14 +4,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { ALCHEMY_KOVAN_API_KEY, PRIVATE_KEY } = process.env;
+const { ALCHEMY_KOVAN_API_KEY, ALCHEMY_RINKEBY_API_KEY, PRIVATE_KEY } = process.env;
 
 module.exports = {
   defaultNetwork: 'hardhat',
   networks: {
+    hardhat: {},
     kovan: {
-      hardhat: {},
       url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_KOVAN_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_RINKEBY_API_KEY}`,
       accounts: [PRIVATE_KEY],
     },
   },
