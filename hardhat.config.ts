130d1fd7618d26833a32c84f8112dc8a44f2301a
Peter Graham <peterghm@gmail.com>
2021-08-28 20:51:32 -1000

    deploy config for kovan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,20 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-vyper';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { ALCHEMY_KOVAN_API_KEY, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: {
     version: '0.8.4',
+    networks: {
+      kovan: {
+        url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_KOVAN_API_KEY}`,
+        accounts: [`0x${PRIVATE_KEY}`],
+      },
+    },
   },
   vyper: {
     version: '0.2.15',
